6fcc97fa12c591b85b45a562200da5439bcd24e0
saitama-sensei <contact.yourwebdev@gmail.com>
2024-10-28 18:12:32 +0530

    structure : added db and action files for better folder structure

---
+++ www/src/server/actions/personaActions.ts
@@ -0,0 +1,11 @@
+"use server"
+
+export async function createPersona() {
+    // actions here
+}
+export async function updatePersona() {
+    // actions here
+}
+export async function deletePersona() {
+    // actions here
+}
---
+++ www/src/server/database/personaQueries.ts
@@ -0,0 +1,7 @@
+// import { PrismaClient, Prisma } from '@prisma/client';
+
+// const db = new PrismaClient();
+
+export async function getPersonaById() {
+    // db query
+}
